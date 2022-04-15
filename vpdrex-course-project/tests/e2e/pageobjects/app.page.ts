4f6f15483f43df94ec8874523682720418a548cc
Paratala <vparat395@resml-1724476.lan>
2022-04-14 21:18:45 -0400

    created project basics

---
+++ vpdrex-course-project/tests/e2e/pageobjects/app.page.ts
@@ -0,0 +1,15 @@
+class App {
+  /**
+   * elements
+   */
+  get heading () { return $('h1') }
+
+  /**
+   * methods
+   */
+  async open (path = '/') {
+    await browser.url(path)
+  }
+}
+
+export default new App()
